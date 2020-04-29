f97d5a008ae90efcfc16ddb64856d2b64f3735b3
SEYEON_PARK <fourleaf123@naver.com>
2020-04-29 20:54:33 +0900

    1주차 과제
    별 다이아몬드 모양으로 출력하기

---
+++ 별.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	for (int i = 1; i < 10; i++)
+	{
+		if (i == 1)
+		{
+			printf("                *\n");
+		}
+		if (i == 2)
+		{
+			printf("               * *\n");
+		}
+		if (i == 3)
+		{
+			printf("              * * *\n");
+		}
+		if (i == 4)
+		{
+			printf("             * * * *\n");
+		}
+		if (i == 5)
+		{
+			printf("            * * * * *\n");
+		}
+		if (i == 6)
+		{
+			printf("           * * * * * *\n");
+		}
+		if (i == 7)
+		{
+			printf("          * * * * * * *\n");
+		}
+		if (i == 8)
+		{
+			printf("         * * * * * * * *\n");
+		}
+		if (i == 9)
+		{
+			printf("        * * * * * * * * *\n");
+		}
+	}
+
+	for (int i = 8; i > 0; i--)
+	{
+		if (i == 1)
+		{
+			printf("                *\n");
+		}
+		if (i == 2)
+		{
+			printf("               * *\n");
+		}
+		if (i == 3)
+		{
+			printf("              * * *\n");
+		}
+		if (i == 4)
+		{
+			printf("             * * * *\n");
+		}
+		if (i == 5)
+		{
+			printf("            * * * * *\n");
+		}
+		if (i == 6)
+		{
+			printf("           * * * * * *\n");
+		}
+		if (i == 7)
+		{
+			printf("          * * * * * * *\n");
+		}
+		if (i == 8)
+		{
+			printf("         * * * * * * * *\n");
+		}
+	}
+
+
+	return 0;
+}
